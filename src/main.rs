bb3227889e877ecb6d7226a09abe654d0a1ebaff
dippa-1 <dominik.helfenstein@gmx.de>
2022-03-09 22:53:01 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use serialport::{available_ports, SerialPortType};
+
+fn main() {
+  let port_name: &str;
+  let baud_rate: u32 = 9600;
+    match available_ports() {
+        Ok(ports) => {
+            match ports.len() {
+                0 => println!("No ports found."),
+                1 => println!("Found 1 port:"),
+                n => println!("Found {} ports:", n),
+            };
+            for p in ports {
+                println!("  {}", p.port_name);
+                match p.port_type {
+                    SerialPortType::UsbPort(info) => {
+                        println!("    Type: USB");
+                        println!("    VID:{:04x} PID:{:04x}", info.vid, info.pid);
+                        println!(
+                            "     Serial Number: {}",
+                            info.serial_number.as_ref().map_or("", String::as_str)
+                        );
+                        println!(
+                            "      Manufacturer: {}",
+                            info.manufacturer.as_ref().map_or("", String::as_str)
+                        );
+                        println!(
+                            "           Product: {}",
+                            info.product.as_ref().map_or("", String::as_str)
+                        );
+                        if info.vid == 0x1111 {
+                          port_name = p.port_name;
+                        }
+                    }
+                    SerialPortType::BluetoothPort => {
+                        println!("    Type: Bluetooth");
+                    }
+                    SerialPortType::PciPort => {
+                        println!("    Type: PCI");
+                    }
+                    SerialPortType::Unknown => {
+                        println!("    Type: Unknown");
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("{:?}", e);
+            eprintln!("Error listing serial ports");
+        }
+    }
+
+    let port = serialport::new(port_name, baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open();
+
+    match port {
+        Ok(mut port) => {
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
+            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+            loop {
+                match port.read(serial_buf.as_mut_slice()) {
+                    Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
+                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
+            ::std::process::exit(1);
+        }
+      }
+        
+
+}
